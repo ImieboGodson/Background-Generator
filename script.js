56847911d7f20c707c0cb6c8e9013607c4ec020a
Deifa <Deifa@users.noreply.github.com>
2020-09-23 18:45:51 +0100

    Added Comment to script.js file

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
+//Testing with node bundle
 let _ = require("lodash");
 
 const array = [1,2,3,4,5,67,8];
 console.log("answer:", _.without(array, 67));
+
+
 let colorSelector1 = document.getElementById("color1");
 let colorSelector2 = document.getElementById("color2");
 let result = document.getElementById("result");
