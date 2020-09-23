1020c48c9595f11bd2b95b78ef78d25494b671ea
Deifa <Deifa@users.noreply.github.com>
2020-09-23 18:40:23 +0100

    Added package.json file

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+let _ = require("lodash");
+
+const array = [1,2,3,4,5,67,8];
+console.log("answer:", _.without(array, 67));
 let colorSelector1 = document.getElementById("color1");
 let colorSelector2 = document.getElementById("color2");
 let result = document.getElementById("result");
